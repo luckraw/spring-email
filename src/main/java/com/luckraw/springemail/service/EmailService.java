a153730441e69a9902694cb1f01d4d6fba60755c
null <krawdev@gmail.com>
2023-09-04 20:49:03 -0300

    Model EmailService created.

---
+++ src/main/java/com/luckraw/springemail/service/EmailService.java
@@ -0,0 +1,29 @@
+package com.luckraw.springemail.service;
+
+import com.luckraw.springemail.model.Email;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmailService {
+
+
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(Email email) throws MessagingException {
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(email.getFrom());
+        helper.setSubject(email.getSubject());
+        helper.setText(email.getBody());
+
+        javaMailSender.send(message);
+    }
+}
