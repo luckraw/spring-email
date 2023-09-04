e3327d31f2d493e63f99e1d056d6c4aa81199acf
null <krawdev@gmail.com>
2023-09-04 10:23:58 -0300

    initial commit

---
+++ src/main/java/com/luckraw/springemail/SpringEmailApplication.java
@@ -0,0 +1,13 @@
+package com.luckraw.springemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEmailApplication.class, args);
+    }
+
+}
